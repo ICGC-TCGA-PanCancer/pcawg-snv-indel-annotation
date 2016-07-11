b5b366a22975c43762888a5bf01b67b61d60110a
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-11 10:45:22 -0400

    Fixed script to only need 1 input

--- scripts/extract_snvs_from_indels.sh
+++ scripts/extract_snvs_from_indels.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
 PATH_TO_INDEL=$1
-PATH_TO_OUTPUT=$2
 
 bgzip -d -c $PATH_TO_INDEL > ${PATH_TO_INDEL}_somatic.indel.bcftools-norm.vcf \
-	&& grep -e '^#' -i -e '^[^#].*[[:space:]][ACTG][[:space:]][ACTG][[:space:]]' ${PATH_TO_INDEL}_somatic.indel.bcftools-norm.vcf > $PATH_TO_OUTPUT \
-	&& bgzip -f $PATH_TO_OUTPUT \
-	&& tabix -f -p vcf $PATH_TO_OUTPUT.gz
+	&& grep -e '^#' -i -e '^[^#].*[[:space:]][ACTG][[:space:]][ACTG][[:space:]]' ${PATH_TO_INDEL}_somatic.indel.bcftools-norm.vcf > ${PATH_TO_INDEL}_somatic.extracted-SNVs.vcf \
+	&& bgzip -f ${PATH_TO_INDEL}_somatic.extracted-SNVs.vcf \
+	&& tabix -f -p vcf ${PATH_TO_INDEL}_somatic.extracted-SNVs.vcf.gz
