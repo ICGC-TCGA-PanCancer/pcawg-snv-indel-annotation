2a2b0d64b2f1afb1f5377fbd825ca10d49cf3a63
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-08-12 15:26:59 -0400

    A number of things:
     - point to new version of variantbam-for-dockstore
     - fixed error in JS files
     - new cwl (and JS) for whole oxog/variantbam/annotate workflow

--- preprocess_util.js
+++ preprocess_util.js
@@ -4,7 +4,7 @@ function filterForIndels(inArr)
 	var arr = [];
 	for (var i = 0; i < inArr.length ; i++)
 	{
-		if (inArr[i].basename.indexOf("indel") > 0)
+		if (inArr[i].basename.indexOf("indel") >= 0)
 		{
 			arr.push(inArr[i]);
 		}
@@ -17,7 +17,7 @@ function filterFor(workflowName, vcfType, inArr)
 	var arr = [];
 	for (var i = 0; i < inArr.length; i++)
 	{
-		if (inArr[i].basename.indexOf(workflowName) > 0 && inArr[i].basename.indexOf(vcfType) > 0)
+		if (inArr[i].basename.indexOf(workflowName) >= 0 && inArr[i].basename.indexOf(vcfType) >= 0)
 		{
 			arr.push(inArr[i])
 		}
